020b5c3133cfc8d615664f5c4e2eca2cd303e498
ShivamVerma07ops <sv739901@gmail.com>
2025-04-23 15:26:01 +0530

    inheritance,prototype

--- Class.js
+++ Class.js
@@ -115,30 +115,88 @@
 //        this. #firstname= firstname;
     //     this.lastname = lastname;
 //// get method ////////////////////////
-class Person{
-        #firstname;
-        #lastname;
+// class Person{
+//         #firstname;
+//         #lastname;
     
-    constructor(firstname,lastname){
-        this. #firstname= firstname;
-        this.lastname = lastname;
+//     constructor(firstname,lastname){
+//         this. #firstname= firstname;
+//         this.lastname = lastname;
     
-    }
-    get firstname(){
-        return this.#firstname;
-    }
-    set firstname(firstname){
-        this.#firstname=firstname;
-    }
-    get lastname(){
-        return this.#lastname;
-    }
-    set lastname(lastname){
-        this.#lastname=lastname;
-    }
-    }
-    let p= new Person("shivam","verma");
-    console.log(p.firstname);
-    p.firstname="hello";
-    console.log(p.firstname);
-    console.log(p);
+//     }
+//     get firstname(){
+//         return this.#firstname;
+//     }
+//     set firstname(firstname){
+//         this.#firstname=firstname;
+//     }
+//     get lastname(){
+//         return this.#lastname;
+//     }
+//     set lastname(lastname){
+//         this.#lastname=lastname;
+//     }
+//     }
+//     let p= new Person("shivam","verma");
+//     console.log(p.firstname);
+//     p.firstname="hello";
+//     console.log(p.firstname);
+//     console.log(p);
+
+
+///// inheritance = reusability of code which is common to all classes that inherit the method aand propertis form the parent calss
+/// single level multilevel multiple level hirearicical (hybrid)
+
+// class Vehical{
+//     constructor(color,currentSpeed,maxSpeed){
+//         this.color= color;
+//         this.currentSpeed= currentSpeed;
+//         this.maxSpeed=maxSpeed;
+//     }
+//     move(){
+//         console.log("arriving at", this.currentSpeed);
+//     }
+//     accelerate(amount){
+//         this.currentSpeed+=amount;
+//     }
+// }
+// ///next
+// class Motorcycle extends Vehical{
+//     constructor(color,currentSpeed,maxSpeed,fuel){
+//         super(color,currentSpeed,maxSpeed);
+//         this.fuel=fuel;
+
+// }
+// doWheelie(){
+//     console.log("Driving  on one wheel");
+// }
+
+// }
+// let motor = new Motorcycle("Black",0,250,"gasolin");
+// console.log(motor.color);
+// motor.accelerate(300);
+
+
+///// PROTOTYPE /////////
+///next
+class Person {
+                constructor(firstname,lastname){
+                    this.firstname = firstname;
+                    this.lastname = lastname;
+            
+                }
+            greet(){
+                console.log("hi there i am" );
+    
+            }
+    
+            }
+            Person.prototype.introduce=function(){
+                console.log("hi i am ",this.firstname)
+            };
+
+        Person.prototype.favcolor="green";
+
+        let p = new Person ("  shivam ", "verma");
+        console.log(p.favcolor);
+        p.introduce();
