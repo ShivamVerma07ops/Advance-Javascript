41bfba4a32106aad04fa36b6f45498b320d73b2f
ShivamVerma07ops <sv739901@gmail.com>
2025-04-29 12:58:53 +0530

    Destrucyuring and Exponentialmethodand Array includes

--- Maps.js
+++ Maps.js
@@ -106,11 +106,78 @@
 // console.log(Person);
 
  //object default
- const Person={
-    firstName:"Shashank",
-    lastName:"dubey",
-    age:"22"
+//  const Person={
+//     firstName:"Shashank",
+//     lastName:"dubey",
+//     age:"22"
 
- };
- let{lastName:name}= Person;
- console.log(Person);
+//  };
+//  let{lastName:name}= Person;
+//  console.log(Person);
+
+
+/// String destructuring
+// let name="GurukulTheSchool"
+// /// Destructuring
+// let[a1,a2,a3,a4,a5]=name;
+// console.log(a1);
+// console.log(a2);
+
+
+/// next
+// const fruit=["banana","apple","mango","orange"];
+// // let[f1,,,f2]=fruit;
+// // console.log(f1,f2);
+// let{[0]:f1,[1]:f2}=fruit;
+// console.log(f1,f2);
+
+
+//// rest property
+// const numbers=[10,20,30,40,50,60,70];
+// const[a,b,...rest]=numbers;
+// console.log(a,b,rest);
+//// swap two variable /////////////
+// let firstname="shivam";
+// let lastname="verma";
+// //destructuring
+// [firstname,lastname]=[lastname,firstname];
+// console.log(firstname,lastname);
+
+/// JAVSCRIPT EXPONENMTIAL FUNCTION
+// let x=5;
+// let z=x**2;
+// console.log(z);
+
+
+//next
+// let x=5;
+// let z= Math.pow(x,2);
+// console.log(z);
+
+
+/// array includes///////////
+
+// const fruit=["banana","apple","mango","orange"];
+// // Start the search at position3;
+// console.log(fruit.includes("mango"));
+// console.log(fruit.includes("banana",3));
+
+
+
+/// traling comma:----
+
+// const arr=[
+//    "one",
+//    "two",
+//    "three",,
+// ];
+// console.log(arr.length);
+
+/// next
+/// SPARSEW
+
+// const sparseArray=[1,,,4,5,,];
+// console.log(sparseArray.length);
+
+const person ={firstname:"shivam,age:30",,,};
+console.log(person);
